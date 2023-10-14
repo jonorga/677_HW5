629afadf80df5f0b1fe2a38e77d5caad24b3104b
jonorgan <jonorgan26@gmail.com>
2023-10-14 15:42:11 -0400

    Finished linear part of assignment

--- hw5_linear.py
+++ hw5_linear.py
@@ -4,6 +4,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+pd.options.mode.chained_assignment = None  # default='warn'
 
 file_cmg = pd.read_csv("cmg_weeks.csv")
 file_spy = pd.read_csv("spy_weeks.csv")
@@ -25,7 +26,7 @@ def GeneratePlot(frame, name, year):
 	axes1.set_title("Average Return vs Volatility for " + name)
 	axes1.set_xlabel("Average Return")
 	axes1.set_ylabel("Volatility")
-	scatter_plot.savefig(name + "_Avg_Return_V_Volatility_Y" + year + ".png")
+	scatter_plot.savefig("linear_results/" + name + "_Avg_Return_V_Volatility_Y" + year + ".png")
 
 
 year_line_cmg = len(clean_cmg.index) / 2
@@ -35,6 +36,11 @@ year_1_cmg = clean_cmg[clean_cmg["Week"] <= year_line_cmg]
 year_2_cmg = clean_cmg[clean_cmg["Week"] > year_line_cmg]
 year_1_spy = clean_spy[clean_spy["Week"] <= year_line_spy]
 year_2_spy = clean_spy[clean_spy["Week"] > year_line_spy]
+
+year_2_cmg.loc[year_2_cmg['Avg_Return'] >= 0, 'Color'] = "Green"
+year_2_cmg.loc[year_2_cmg['Avg_Return'] < 0, 'Color'] = "Red"
+year_2_spy.loc[year_2_spy['Avg_Return'] >= 0, 'Color'] = "Green"
+year_2_spy.loc[year_2_spy['Avg_Return'] < 0, 'Color'] = "Red"
 
 GeneratePlot(year_1_cmg, "CMG", "1")
 GeneratePlot(year_2_cmg, "CMG", "2")
