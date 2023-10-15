aafa20f4ae47835be080dc4bfbd00fbe228cd6ee
jonorgan <jonorgan26@gmail.com>
2023-10-15 16:56:18 -0400

    Started working on Oracle assignment

---
+++ hw5_oracle.py
@@ -0,0 +1,34 @@
+###
+### CS667 Data Science with Python, Homework 5, Jon Organ
+###
+
+# TODO: Get CMG and SPY files
+
+
+# Question 1.1 =================================================================================================
+# TODO: For each of the five years
+	# TODO: Compute mean and standard deviation for all weeks, negative weeks, and non negative weeks
+
+
+# Question 1.2 =================================================================================================
+# TODO: take data from question 1.1 and generate tables for each year
+
+
+# Question 1.3 =================================================================================================
+# TODO: Are there more weeks with negative or non-negative returns in total?
+
+
+# Question 1.4 =================================================================================================
+# TODO: Is the absolute value of the average returns greater for negative or non-negative returns?
+
+
+# Question 2 =================================================================================================
+# TODO: Answers from analyzing tables
+
+
+# Question 3 =================================================================================================
+# TODO: Compute aggregate table across all 5 years
+
+
+# Question 3.1 =================================================================================================
+# TODO:
