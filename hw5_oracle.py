81aa1af85afe59a8cd40cae3c961373ab108c5f8
jonorgan <jonorgan26@gmail.com>
2023-10-15 17:23:55 -0400

    Outline for oracle finished

--- hw5_oracle.py
+++ hw5_oracle.py
@@ -27,8 +27,68 @@
 
 
 # Question 3 =================================================================================================
-# TODO: Compute aggregate table across all 5 years
+# TODO: Compute aggregate table across all 5 years for both stocks
 
 
 # Question 3.1 =================================================================================================
-# TODO:
+# TODO: What are the best and worst weeks from tables generated in last question
+
+
+# Question 3.2 =================================================================================================
+# TODO: Are these the same weeks for both stocks?
+
+
+# Question 3.3 =================================================================================================
+# TODO: For each of the weeks in your original weeks file, how many were outside the standard deviation?
+	# See PDF for equation
+
+
+# Question 4.1 =================================================================================================
+# TODO: You trade each week perfectly with CMG, starting with $100, how much do you make after 5 years?
+
+
+# Question 4.2 =================================================================================================
+# TODO: Same as 4.1 except for SPY
+
+
+# Question 4.3 =================================================================================================
+# TODO: How long would it take for your stock and SPY to make $176 starting from $100
+
+
+# Question 5.1 =================================================================================================
+# TODO: for both stocks, how much will you make with buy and hold?
+
+
+# Question 5.2 =================================================================================================
+# TODO: How does the results of 5.1 compare to 4
+
+
+# Question 5.3 =================================================================================================
+# TODO: Buy and hold except you put it into cash at end of may, and back into stock on last day of august
+	# Summarize in table, see PDF
+
+
+# Question 5.4 =================================================================================================
+# TODO: Both stocks, buy and hold 12 times, except each time, for one month of the year, sell on the first trading
+# day of that month, and buy on the last trading day of that month
+	# Summarize in table, see PDF
+
+
+# Question 6 =================================================================================================
+# TODO: For both stocks trade each week with the oracle
+# Generate random number r between 0 and 1, if p >= r, the oracle predicts true value, otherwise opposite
+# Generate table doing this ten times for p = 0 to 1 with 0.1 steps
+
+
+# Question 6.1 =================================================================================================
+# TODO: What value of p equals buy and hold results?
+
+
+# Question 6.2 =================================================================================================
+# TODO: Any patterns in table from question 6?
+
+
+# Question 7.1 =================================================================================================
+# TODO: Compute table from question
+
+
